44d7cfb4a000c09dd0901b2aeba6f61d50ec2970
Kevin Mulvey <kmulvey@linux.com>
2023-03-14 17:57:51 -0600

    add more convert ops
    Signed-off-by: Kevin Mulvey <kmulvey@linux.com>

--- pkg/imageconvert/worker_test.go
+++ pkg/imageconvert/worker_test.go
@@ -16,6 +16,8 @@ func TestConvertImage(t *testing.T) {
 	var testdir = makeTestDir(t)
 	var testImage = moveImage(t, testdir, testPair{Name: "./testimages/test.webp", Type: "jpeg"})
 	var ic, err = NewWithDefaults(testImage, "", 0)
+	ic.WithCompression()
+	ic.WithResize(200, 100, 300, 200)
 	assert.NoError(t, err)
 
 	originalFile, err := path.NewEntry(testImage, 0)
@@ -27,9 +29,9 @@ func TestConvertImage(t *testing.T) {
 	assert.True(t, strings.HasSuffix(cr.OriginalFileName, "test.webp"))
 	assert.True(t, strings.HasSuffix(cr.ConvertedFileName, "test.jpg"))
 	assert.Equal(t, "webp", cr.ImageType)
-	assert.False(t, cr.Compressed)
+	assert.True(t, cr.Compressed)
 	assert.False(t, cr.Renamed)
-	assert.False(t, cr.Resized)
+	assert.True(t, cr.Resized)
 
 	assert.NoError(t, os.RemoveAll(testdir))
 }
