e11d81891ae6506be87837b7ba65b83f197026bc
Kevin Mulvey <kmulvey@linux.com>
2023-03-14 17:32:55 -0600

    test With funcs
    Signed-off-by: Kevin Mulvey <kmulvey@linux.com>

--- pkg/imageconvert/imageconvert_test.go
+++ pkg/imageconvert/imageconvert_test.go
@@ -3,7 +3,9 @@ package imageconvert
 import (
 	"os"
 	"testing"
+	"time"
 
+	"github.com/kmulvey/humantime"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,9 +14,33 @@ func TestNewWithDefaults(t *testing.T) {
 
 	// setup
 	var testdir = makeTestDir(t)
-	var ic, err = NewWithDefaults("noexist", "", 0)
-	assert.Error(t, err)
+	var testImage = moveImage(t, testdir, testPair{Name: "./testimages/realjpg.jpg", Type: "jpeg"})
+
+	var ic, err = NewWithDefaults(testImage, "", 0)
+	assert.NoError(t, err)
 	assert.Equal(t, uint8(1), ic.Threads)
+
+	ic.WithCompression()
+	assert.True(t, ic.Compress)
+
+	ic.WithWatch()
+	assert.True(t, ic.Watch)
+
+	ic.WithForce()
+	assert.True(t, ic.Force)
+
+	ic.WithResize(200, 100, 300, 200)
+	assert.Equal(t, uint16(200), ic.ResizeWidth)
+	assert.Equal(t, uint16(300), ic.ResizeWidthThreshold)
+	assert.Equal(t, uint16(100), ic.ResizeHeight)
+	assert.Equal(t, uint16(200), ic.ResizeHeightThreshold)
+
+	ic.WithThreads(3)
+	assert.Equal(t, uint8(3), ic.Threads)
+
+	ic.WithTimeRange(humantime.TimeRange{From: time.Time{}, To: time.Now()})
+	assert.Equal(t, time.Time{}, ic.TimeRange.From)
+	assert.Equal(t, time.Now().Day(), ic.TimeRange.To.Day())
 
 	assert.NoError(t, os.RemoveAll(testdir))
 }
