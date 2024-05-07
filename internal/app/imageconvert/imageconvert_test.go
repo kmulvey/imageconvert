53aaa99aa4b53ba780b17ca1738d007c9caf10ad
Kevin Mulvey <kmulvey@linux.com>
2024-05-07 12:03:22 -0600

    TestNewImageConverterGood

--- internal/app/imageconvert/imageconvert_test.go
+++ internal/app/imageconvert/imageconvert_test.go
@@ -5,66 +5,40 @@ import (
 	"testing"
 	"time"
 
-	"github.com/kmulvey/humantime"
+	"github.com/kmulvey/path"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewWithDefaults(t *testing.T) {
+func TestNewImageConverterGood(t *testing.T) {
 	t.Parallel()
 
-	// setup
 	var testdir = makeTestDir(t)
 	var testImage = moveImage(t, testdir, testPair{Name: "./testimages/realjpg.jpg", Type: "jpeg"})
-
-	var ic, err = NewImageConverter(testImage, "", 0)
-	assert.Equal(t, uint8(1), ic.Threads)
+	var testImageEntry, err = path.NewEntry(testImage, 0)
 	assert.NoError(t, err)
 
-	ic.WithCompression()
-	assert.True(t, ic.Compress)
-
-	ic.WithWatch()
-	assert.True(t, ic.Watch)
-
-	ic.WithForce()
-	assert.True(t, ic.Force)
-
-	ic.WithResize(200, 100, 300, 200)
-	assert.Equal(t, uint16(200), ic.ResizeWidth)
-	assert.Equal(t, uint16(300), ic.ResizeWidthThreshold)
-	assert.Equal(t, uint16(100), ic.ResizeHeight)
-	assert.Equal(t, uint16(200), ic.ResizeHeightThreshold)
-
-	ic.WithThreads(3)
-	assert.Equal(t, uint8(3), ic.Threads)
-
-	ic.WithTimeRange(humantime.TimeRange{From: time.Time{}, To: time.Now()})
+	var config = &ImageConverterConfig{
+		OriginalImages: []string{testImage},
+		Threads:        1,
+		Quality:        30,
+	}
+	ic, err := NewImageConverter(config)
+	assert.NoError(t, err)
+	assert.Equal(t, []path.Entry{testImageEntry}, ic.OriginalImagesEntries)
+	assert.Equal(t, "", ic.WatchDir)
+	assert.Equal(t, 1, ic.Threads)
+	assert.Equal(t, "processed.log", ic.SkipMapFileHandle.Name())
+	assert.Equal(t, 0, len(ic.SkipMap))
+	assert.Equal(t, 30, ic.Quality)
+	assert.False(t, ic.Force)
+	assert.False(t, ic.DeleteOriginal)
 	assert.Equal(t, time.Time{}, ic.TimeRange.From)
-	assert.Equal(t, time.Now().Day(), ic.TimeRange.To.Day())
+	assert.Equal(t, time.Time{}, ic.TimeRange.To)
+	assert.Equal(t, uint16(0), ic.ResizeWidth)
+	assert.Equal(t, uint16(0), ic.ResizeHeight)
+	assert.Equal(t, uint16(0), ic.ResizeWidthThreshold)
+	assert.Equal(t, uint16(0), ic.ResizeHeightThreshold)
 
-	assert.NoError(t, os.RemoveAll(testdir))
-}
-
-func TestStartSlice(t *testing.T) {
-	t.Parallel()
-
-	// setup
-	var testdir = makeTestDir(t)
-
-	var ic, err = NewImageConverter(testdir, "", 1)
-	assert.NoError(t, err)
-	assert.Equal(t, uint8(1), ic.Threads)
-	ic.WithCompression()
-
-	compressedTotal, renamedTotal, resizedTotal, totalFiles, conversionTypeTotals, err := ic.Start(nil)
-	assert.NoError(t, err)
-
-	assert.Equal(t, 3, compressedTotal)
-	assert.Equal(t, 0, renamedTotal)
-	assert.Equal(t, 0, resizedTotal)
-	assert.Equal(t, 4, totalFiles)
-	assert.EqualValues(t, map[string]int{"jpeg": 1, "png": 1, "webp": 1}, conversionTypeTotals)
-
-	assert.NoError(t, os.RemoveAll("processed.log"))
+	assert.NoError(t, ic.Shutdown())
 	assert.NoError(t, os.RemoveAll(testdir))
 }
