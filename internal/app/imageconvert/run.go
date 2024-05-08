4e93c4b45fa2fcedc87d353ae2d3fd1bb2ac6853
Kevin Mulvey <kmulvey@linux.com>
2024-05-07 19:13:59 -0600

    split wtach and start

--- internal/app/imageconvert/run.go
+++ internal/app/imageconvert/run.go
@@ -6,76 +6,74 @@ import (
 	"github.com/kmulvey/path"
 )
 
-// Start begins the conversion process and returns counts of each type of operation preformed.
-func (ic *ImageConverter) Start(ctx context.Context, results chan ConversionResult) (int, int, error) {
+func (ic *ImageConverter) Watch(ctx context.Context, results chan ConversionResult) {
 
-	if ic.WatchDir != "" {
-		var originalImages = make(chan path.WatchEvent)
-		var errors = make(chan error)
-		var done = make(chan struct{})
+	var originalImages = make(chan path.WatchEvent)
+	var errors = make(chan error)
+	var done = make(chan struct{})
 
-		go func() {
-			var errorsOpen, originalImagesOpen = true, true
-			for errorsOpen || originalImagesOpen {
-				select {
+	go func() {
+		var errorsOpen, originalImagesOpen = true, true
+		for errorsOpen || originalImagesOpen {
+			select {
 
-				case err, open := <-errors:
-					if !open {
-						errorsOpen = false
-						continue
-					}
-					results <- ConversionResult{
-						Error: err,
-					}
+			case err, open := <-errors:
+				if !open {
+					errorsOpen = false
+					continue
+				}
+				results <- ConversionResult{
+					Error: err,
+				}
 
-				case originalImage, open := <-originalImages:
-					if !open {
-						originalImagesOpen = false
-						continue
-					}
-					var _, exists = ic.SkipMap[originalImage.AbsolutePath]
+			case originalImage, open := <-originalImages:
+				if !open {
+					originalImagesOpen = false
+					continue
+				}
+				var _, exists = ic.SkipMap[originalImage.AbsolutePath]
 
-					if (!originalImage.Entry.FileInfo.IsDir() && !exists) || ic.Force {
+				if (!originalImage.Entry.FileInfo.IsDir() && !exists) || ic.Force {
 
-						var result = ic.convertImage(originalImage.Entry)
-						results <- ic.convertImage(originalImage.Entry)
-						if result.Error == nil {
-							ic.SkipMapFileHandle.WriteString(originalImage.AbsolutePath + "\n")
-							ic.SkipMap[originalImage.AbsolutePath] = struct{}{}
-						}
+					var result = ic.convertImage(originalImage.Entry)
+					results <- ic.convertImage(originalImage.Entry)
+					if result.Error == nil {
+						ic.SkipMapFileHandle.WriteString(originalImage.AbsolutePath + "\n")
+						ic.SkipMap[originalImage.AbsolutePath] = struct{}{}
 					}
 				}
 			}
-			close(done)
-		}()
-
-		go path.WatchDir(ctx, ic.WatchDir, ic.Depth, true, originalImages, errors, path.NewDateWatchFilter(ic.TimeRange.From, ic.TimeRange.To))
-		<-done
-
-	} else {
-
-		var resizedTotal, totalProcessed int
-		for i, originalImage := range ic.OriginalImagesEntries {
-
-			var _, exists = ic.SkipMap[originalImage.AbsolutePath]
-
-			if (!originalImage.FileInfo.IsDir() && !exists) || ic.Force {
-				var result = ic.convertImage(originalImage)
-				if result.Error != nil {
-					return 0, 0, result.Error
-				}
-				ic.SkipMapFileHandle.WriteString(originalImage.AbsolutePath + "\n")
-				ic.SkipMap[originalImage.AbsolutePath] = struct{}{}
-
-				if result.Resized {
-					resizedTotal++
-				}
-			}
-			totalProcessed = i
 		}
+		close(done)
+	}()
 
-		return totalProcessed, resizedTotal, nil
+	go path.WatchDir(ctx, ic.WatchDir, ic.Depth, true, originalImages, errors, path.NewDateWatchFilter(ic.TimeRange.From, ic.TimeRange.To))
+	<-done
+}
+
+// Start begins the conversion process and returns counts of each type of operation preformed.
+func (ic *ImageConverter) Start() (int, int, error) {
+
+	var resizedTotal, processedTotal int
+	for i, originalImage := range ic.OriginalImagesEntries {
+
+		var _, exists = ic.SkipMap[originalImage.AbsolutePath]
+
+		if (!originalImage.FileInfo.IsDir() && !exists) || ic.Force {
+			var result = ic.convertImage(originalImage)
+			if result.Error != nil {
+				return 0, 0, result.Error
+			}
+
+			ic.SkipMapFileHandle.WriteString(originalImage.AbsolutePath + "\n")
+			ic.SkipMap[originalImage.AbsolutePath] = struct{}{}
+
+			if result.Resized {
+				resizedTotal++
+			}
+			processedTotal = i + 1
+		}
 	}
 
-	return 0, 0, nil
+	return processedTotal, resizedTotal, nil
 }
