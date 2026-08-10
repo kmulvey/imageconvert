762dc90e8c1a899312d7a51e3a57cfff7d35e917
Kevin Mulvey <kmulvey@linux.com>
2026-08-10 11:14:52 -0600

    make compress check threaded

--- cmd/compresscheck/main.go
+++ cmd/compresscheck/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/kmulvey/humantime"
@@ -13,6 +15,13 @@ import (
 	"go.szostok.io/version/printer"
 )
 
+type quality struct {
+	aboveThreshold bool
+	currentQuality int
+	inputPath      string
+	err            error
+}
+
 func main() {
 	log.SetFormatter(&log.TextFormatter{
 		FullTimestamp:   true,
@@ -20,14 +29,15 @@ func main() {
 	})
 
 	var inputPath, processedLogFile string
-	var quality, directoryDepth int
+	var qualityThreshold, directoryDepth, threads int
 	var timerange humantime.TimeRange
 	var v, h bool
 
 	flag.StringVar(&inputPath, "path", "", "path to files, globbing must be quoted")
 	flag.StringVar(&processedLogFile, "processed-file", "processed.log", "the file previously processed images were written to")
-	flag.IntVar(&quality, "quality", 90, "quality threshold: files at or above this quality will be reported as compressible")
+	flag.IntVar(&qualityThreshold, "quality", 90, "quality threshold: files at or above this quality will be reported as compressible")
 	flag.IntVar(&directoryDepth, "depth", 1, "number of levels to search directories for images")
+	flag.IntVar(&threads, "threads", 1, "number of threads to use for quality checking")
 	flag.Var(&timerange, "time-range", "only consider files changed within this time range")
 	flag.BoolVar(&v, "version", false, "print version")
 	flag.BoolVar(&v, "v", false, "print version")
@@ -48,7 +58,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	log.Infof("Config: dir: %s, log file: %s, quality threshold: %d, modified-since: %s", inputPath, processedLogFile, quality, timerange)
+	log.Infof("Config: dir: %s, log file: %s, quality threshold: %d, modified-since: %s", inputPath, processedLogFile, qualityThreshold, timerange)
 
 	var configs []app.ConfigFunc
 	if timerange.From != app.NilTime || timerange.To != app.NilTime {
@@ -62,17 +72,73 @@ func main() {
 	}
 
 	var compressible int
-	for _, entry := range ic.InputFiles {
-		aboveThreshold, currentQuality, err := imageconvert.QualityCheck(quality, entry.AbsolutePath)
-		if err != nil {
-			log.Warnf("skipping %s: %s", entry.AbsolutePath, err)
+	var f, _ = os.Create("compresscheck.log")
+	var wg = sync.WaitGroup{}
+	var ctx = context.Background()
+	var images = make(chan string)
+	var results = make(chan quality)
+
+	for range threads {
+		wg.Add(1)
+		go qualityCheck(ctx, &wg, qualityThreshold, images, results)
+	}
+
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		for _, entry := range ic.InputFiles {
+			images <- entry.AbsolutePath
+		}
+		close(images)
+		wg.Done()
+	}(&wg)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for result := range results {
+		if result.err != nil {
+			log.Warnf("skipping %s: %s", result.inputPath, result.err)
 			continue
 		}
-		if aboveThreshold {
+		if result.aboveThreshold {
 			compressible++
-			log.Infof("compressible: %s (current quality: %d, would save ~%d%%)", entry.AbsolutePath, currentQuality, currentQuality-quality)
+			log.Infof("compressible: %s (current quality: %d, would save ~%d%%)", result.inputPath, result.currentQuality, result.currentQuality-qualityThreshold)
+			f.WriteString(result.inputPath + "\n")
 		}
 	}
 
-	log.Infof("Done: %d of %d files are compressible at quality threshold %d", compressible, len(ic.InputFiles), quality)
+	f.Close()
+	log.Infof("Done: %d of %d files are compressible at quality threshold %d", compressible, len(ic.InputFiles), qualityThreshold)
+}
+
+func qualityCheck(ctx context.Context, wg *sync.WaitGroup, qualityThreshold int, images chan string, results chan quality) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case image, open := <-images:
+			if !open {
+				return
+			}
+
+			aboveThreshold, currentQuality, err := imageconvert.QualityCheck(qualityThreshold, image)
+			if err != nil {
+				results <- quality{
+					inputPath: image,
+					err:       err,
+				}
+			} else {
+				results <- quality{
+					inputPath:      image,
+					aboveThreshold: aboveThreshold,
+					currentQuality: currentQuality,
+				}
+			}
+		}
+	}
 }
